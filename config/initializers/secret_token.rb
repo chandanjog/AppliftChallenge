c3a9e7daa18d37f3de8b87381fb45aa35a0614cb
chandan jog <jog.chandan@gmail.com>
2014-01-12 15:25:36 +0530

    skeleton project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppliftChallenge::Application.config.secret_key_base = 'f578fdfb125e050e9c58daceb581e0a28e97214789f397bc79ffca93ff66d2c466cbd2660b0efb922a597148da8680f26f62c8f16b160f65498f9841ebbf8ec8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppliftChallenge::Application.config.session_store :cookie_store, key: '_applift-challenge_session'
